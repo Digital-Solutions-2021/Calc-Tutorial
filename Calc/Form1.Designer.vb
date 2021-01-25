6dde7ad27e2b6b07170d5e5931630c2888bfe94f
Charlie McMahon <51872183+charliekmcmahon@users.noreply.github.com>
2021-01-25 19:59:23 +1000

    Add math function

--- Calc/Form1.Designer.vb
+++ Calc/Form1.Designer.vb
@@ -24,6 +24,15 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnSubtract = New System.Windows.Forms.Button()
+        Me.btnMultiply = New System.Windows.Forms.Button()
+        Me.btnDivide = New System.Windows.Forms.Button()
+        Me.txtValueOne = New System.Windows.Forms.TextBox()
+        Me.txtValue2 = New System.Windows.Forms.TextBox()
+        Me.txtAnswer = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Label1
@@ -46,11 +55,95 @@ Partial Class Form1
         Me.btnAdd.Text = "Add"
         Me.btnAdd.UseVisualStyleBackColor = True
         '
+        'btnSubtract
+        '
+        Me.btnSubtract.Location = New System.Drawing.Point(565, 222)
+        Me.btnSubtract.Name = "btnSubtract"
+        Me.btnSubtract.Size = New System.Drawing.Size(141, 61)
+        Me.btnSubtract.TabIndex = 2
+        Me.btnSubtract.Text = "Subtract"
+        Me.btnSubtract.UseVisualStyleBackColor = True
+        '
+        'btnMultiply
+        '
+        Me.btnMultiply.Location = New System.Drawing.Point(565, 303)
+        Me.btnMultiply.Name = "btnMultiply"
+        Me.btnMultiply.Size = New System.Drawing.Size(141, 61)
+        Me.btnMultiply.TabIndex = 3
+        Me.btnMultiply.Text = "Multiply"
+        Me.btnMultiply.UseVisualStyleBackColor = True
+        '
+        'btnDivide
+        '
+        Me.btnDivide.Location = New System.Drawing.Point(565, 377)
+        Me.btnDivide.Name = "btnDivide"
+        Me.btnDivide.Size = New System.Drawing.Size(141, 61)
+        Me.btnDivide.TabIndex = 4
+        Me.btnDivide.Text = "Divide"
+        Me.btnDivide.UseVisualStyleBackColor = True
+        '
+        'txtValueOne
+        '
+        Me.txtValueOne.Location = New System.Drawing.Point(190, 183)
+        Me.txtValueOne.Name = "txtValueOne"
+        Me.txtValueOne.Size = New System.Drawing.Size(195, 20)
+        Me.txtValueOne.TabIndex = 5
+        '
+        'txtValue2
+        '
+        Me.txtValue2.Location = New System.Drawing.Point(190, 263)
+        Me.txtValue2.Name = "txtValue2"
+        Me.txtValue2.Size = New System.Drawing.Size(195, 20)
+        Me.txtValue2.TabIndex = 6
+        '
+        'txtAnswer
+        '
+        Me.txtAnswer.Location = New System.Drawing.Point(190, 377)
+        Me.txtAnswer.Name = "txtAnswer"
+        Me.txtAnswer.Size = New System.Drawing.Size(195, 20)
+        Me.txtAnswer.TabIndex = 7
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(190, 164)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(43, 13)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "Value 1"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(190, 247)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(43, 13)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "Value 2"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(190, 361)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(42, 13)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "Answer"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(808, 496)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtAnswer)
+        Me.Controls.Add(Me.txtValue2)
+        Me.Controls.Add(Me.txtValueOne)
+        Me.Controls.Add(Me.btnDivide)
+        Me.Controls.Add(Me.btnMultiply)
+        Me.Controls.Add(Me.btnSubtract)
         Me.Controls.Add(Me.btnAdd)
         Me.Controls.Add(Me.Label1)
         Me.Name = "Form1"
@@ -62,4 +155,13 @@ Partial Class Form1
 
     Friend WithEvents Label1 As Label
     Friend WithEvents btnAdd As Button
+    Friend WithEvents btnSubtract As Button
+    Friend WithEvents btnMultiply As Button
+    Friend WithEvents btnDivide As Button
+    Friend WithEvents txtValueOne As TextBox
+    Friend WithEvents txtValue2 As TextBox
+    Friend WithEvents txtAnswer As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
 End Class
--- Calc/Form1.vb
+++ Calc/Form1.vb
@@ -1,9 +1,45 @@
-﻿Public Class Form1
+﻿' Code by Charlie McMahon - 2021
+Public Class Form1
+
+    Function Calculator(number1 As Double, number2 As Double, method As Integer) As Double
+        If method = 1 Then
+            ' Addition
+            Return (number1 + number2)
+        ElseIf method = 2 Then
+            ' Subtraction
+            Return (number1 - number2)
+        ElseIf method = 3 Then
+            ' Multipication
+            Return (number1 * number2)
+        ElseIf method = 4 Then
+            ' Division
+            Return (number1 / number2)
+        End If
+    End Function
+
     Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
 
     End Sub
 
     Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
-        MsgBox("The button has been clicked!")
+        ' Add Button
+        Dim iNumber1 As Integer
+        Dim iNumber2 As Integer
+        Dim iAns As Integer
+
+        ' Input path
+        iNumber1 = txtValueOne.Text
+        iNumber2 = txtValue2.Text
+
+        ' Processing Path
+        iAns = Calculator(iNumber1, iNumber2, 1)
+
+        ' Output
+        txtAnswer.Text = iAns
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
     End Sub
 End Class
